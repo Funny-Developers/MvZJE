cebb30ac030b882c87bab3a0056d9bb7df986e21
crazy-piggy <57442208+crazy-piggy@users.noreply.github.com>
2021-10-03 18:20:44 +0800

    update some errors

---
+++ src/main/java/org/funnydevelopers/mvzje/DebugHud.java
@@ -0,0 +1,10 @@
+package org.funnydevelopers.mvzje;
+
+import org.funnydevelopers.mvzje.Main;
+import org.lwjgl.BufferUtils;
+import java.nio.DoubleBuffer;
+import static org.lwjgl.glfw.GLFW.glfwGetCursorPos;
+
+public class DebugHud {
+
+}
--- src/main/java/org/funnydevelopers/mvzje/Main.java
+++ src/main/java/org/funnydevelopers/mvzje/Main.java
@@ -1,12 +1,20 @@
 package org.funnydevelopers.mvzje;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import org.overrun.glutils.GLProgram;
+import org.overrun.glutils.ShaderReader;
 import org.overrun.glutils.wnd.Framebuffer;
 import org.overrun.glutils.wnd.GLFWindow;
 
+import java.nio.DoubleBuffer;
+
+import static java.awt.Font.MONOSPACED;
+import static java.awt.Font.PLAIN;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL13C.GL_CLAMP_TO_BORDER;
 
 public class Main implements Runnable, AutoCloseable {
     public GLFWindow window;
@@ -15,6 +23,20 @@ public class Main implements Runnable, AutoCloseable {
         try (Main main = new Main()) {
             main.start();
         }
+    }
+
+    public static double getCursorPosX(GLFWindow window) {
+        DoubleBuffer posX = BufferUtils.createDoubleBuffer(1);
+        glfwGetCursorPos(window, posX, null);
+        return posX.get(0);
+    }
+    public static double getCursorPosY(GLFWindow window){
+        DoubleBuffer posY = BufferUtils.createDoubleBuffer(1);
+        glfwGetCursorPos(window, posY, null);
+        return posY.get(0);
+    }
+
+    private static void glfwGetCursorPos(GLFWindow window, DoubleBuffer posX, Object o) {
     }
 
     public void resize(int w, int h) {
@@ -45,6 +67,20 @@ public class Main implements Runnable, AutoCloseable {
         resize(800, 600);
         glfwSwapInterval(1);
         run();
+        ClassLoader cl = Main.class.getClassLoader();
+        GLProgram prg = new GLProgram();
+        try {
+            prg.createVsh(ShaderReader.lines(cl, "shaders/scene.vsh"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            prg.createFsh(ShaderReader.lines(cl, "shaders/scene.fsh"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        prg.link();
+        render();
     }
 
     @Override
